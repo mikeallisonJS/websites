9659960892515c8658143616e44af317e53f395e
Mike Allison <dj.mikeallison@gmail.com>
2023-03-23 05:13:42 +0000

    devcontainer update

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,11 @@
 npm install -g npm@latest gulp
 npm install
+
 sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
 curl -sLf --retry 3 --tlsv1.2 --proto "=https" 'https://packages.doppler.com/public/cli/gpg.DE2A7741A397C129.key' | sudo apt-key add -
 echo "deb https://packages.doppler.com/public/cli/deb/debian any-version main" | sudo tee /etc/apt/sources.list.d/doppler-cli.list
-sudo apt-get update && sudo apt-get install doppler jq
+sudo apt-get update && sudo apt-get install -y doppler jq
+
+rm -rf ~/.oh-my-zsh/custom/plugins/nx-completion
 git clone https://github.com/jscutlery/nx-completion.git ~/.oh-my-zsh/custom/plugins/nx-completion
 cp .devcontainer/.zshrc ~/.zshrc
