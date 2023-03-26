b01492f2be4ec49aca8b0a69f7bfb3108fcc7ed7
Mike Allison <dj.mikeallison@gmail.com>
2023-03-26 04:31:17 +0000

    borken

--- packages/members.captainssounds.com/src/app/auth.service.ts
+++ packages/members.captainssounds.com/src/app/auth.service.ts
@@ -1,13 +1,14 @@
 import { Injectable, NgZone } from '@angular/core'
 import * as auth from 'firebase/auth'
+import { Firestore, collection, collectionData } from '@angular/fire/firestore'
 import { AngularFireAuth } from '@angular/fire/compat/auth'
-import {
-  AngularFirestore,
-  AngularFirestoreDocument
-} from '@angular/fire/compat/firestore'
+// import {
+//   AngularFirestore,
+//   AngularFirestoreDocument
+// } from '@angular/fire/compat/firestore'
 import { Router } from '@angular/router'
 
-export interface User {
+export interface UserData {
   uid: string
   email: string
   displayName: string
@@ -19,9 +20,9 @@ export interface User {
   providedIn: 'root'
 })
 export class AuthService {
-  userData: any // Save logged in user data
+  userData: UserData
   constructor(
-    public afs: AngularFirestore, // Inject Firestore service
+    public afs: Firestore, // Inject Firestore service
     public afAuth: AngularFireAuth, // Inject Firebase auth service
     public router: Router,
     public ngZone: NgZone // NgZone service to remove outside scope warning
@@ -30,92 +31,71 @@ export class AuthService {
     logged in and setting up null when logged out */
     this.afAuth.authState.subscribe((user) => {
       if (user) {
-        this.userData = user
+        this.userData = user as UserData
         localStorage.setItem('user', JSON.stringify(this.userData))
-        JSON.parse(localStorage.getItem('user')!)
-      } else {
-        localStorage.setItem('user', 'null')
-        JSON.parse(localStorage.getItem('user')!)
+        // return JSON.parse(localStorage.getItem('user')!)
       }
+      localStorage.removeItem('user')
+      // JSON.parse(localStorage.getItem('user')!)
     })
   }
   // Sign in with email/password
-  SignIn(email: string, password: string) {
-    return this.afAuth
+  async SignIn(email: string, password: string) {
+    const result = await this.afAuth
       .signInWithEmailAndPassword(email, password)
-      .then((result) => {
-        this.SetUserData(result.user)
-        this.afAuth.authState.subscribe((user) => {
-          if (user) {
-            this.router.navigate(['dashboard'])
-          }
-        })
-      })
       .catch((error) => {
         window.alert(error.message)
       })
-  }
-  // Sign up with email/password
-  SignUp(email: string, password: string) {
-    return this.afAuth
-      .createUserWithEmailAndPassword(email, password)
-      .then((result) => {
-        /* Call the SendVerificaitonMail() function when new user sign 
-        up and returns promise */
-        this.SendVerificationMail()
-        this.SetUserData(result.user)
-      })
-      .catch((error) => {
-        window.alert(error.message)
-      })
-  }
-  // Send email verfificaiton when new user sign up
-  SendVerificationMail() {
-    return this.afAuth.currentUser
-      .then((u: any) => u.sendEmailVerification())
-      .then(() => {
-        this.router.navigate(['verify-email-address'])
-      })
-  }
-  // Reset Forggot password
-  ForgotPassword(passwordResetEmail: string) {
-    return this.afAuth
-      .sendPasswordResetEmail(passwordResetEmail)
-      .then(() => {
-        window.alert('Password reset email sent, check your inbox.')
-      })
-      .catch((error) => {
-        window.alert(error)
-      })
-  }
-  // Returns true when user is looged in and email is verified
-  get isLoggedIn(): boolean {
-    const user = JSON.parse(localStorage.getItem('user')!)
-    return user !== null && user.emailVerified !== false ? true : false
-  }
-  // Sign in with Google
-  GoogleAuth() {
-    return this.AuthLogin(new auth.GoogleAuthProvider()).then((res: any) => {
-      this.router.navigate(['dashboard'])
+    if (result?.user == null) return result
+    this.setUserData(result.user)
+    this.afAuth.authState.subscribe((user) => {
+      if (user) {
+        this.router.navigate(['products'])
+      }
     })
   }
-  // Auth logic to run auth providers
-  AuthLogin(provider: any) {
-    return this.afAuth
-      .signInWithPopup(provider)
-      .then((result) => {
-        this.router.navigate(['dashboard'])
-        this.SetUserData(result.user)
-      })
-      .catch((error) => {
-        window.alert(error)
-      })
+  // Sign up with email/password
+  async signUp(email: string, password: string) {
+    const result = await this.afAuth
+      .createUserWithEmailAndPassword(email, password)
+      .catch((error) => window.alert(error))
+    if (result?.user == null) return result
+    this.sendVerificationMail()
+    this.setUserData(result.user)
+    return result
   }
-  /* Setting up user data when sign in with username/password, 
-  sign up with username/password and sign in with social auth  
-  provider in Firestore database using AngularFirestore + AngularFirestoreDocument service */
-  SetUserData(user: any) {
-    const userRef: AngularFirestoreDocument<any> = this.afs.doc(
+  async sendVerificationMail() {
+    const currentUser = await this.afAuth.currentUser
+    await currentUser?.sendEmailVerification()
+    this.router.navigate(['verify-email-address'])
+  }
+  async forgotPassword(passwordResetEmail: string) {
+    await this.afAuth
+      .sendPasswordResetEmail(passwordResetEmail)
+      .catch((error) => window.alert(error))
+    window.alert('Password reset email sent, check your inbox.')
+  }
+  get isLoggedIn(): boolean {
+    const userObj = localStorage.getItem('user')
+    if (userObj == null) return false
+    const user = JSON.parse(userObj)
+    return user.emailVerified !== false ? true : false
+  }
+  googleAuth() {
+    return this.authLogin(new auth.GoogleAuthProvider()).then(() => {
+      this.router.navigate(['products'])
+    })
+  }
+  async authLogin(provider: any) {
+    const result = await this.afAuth
+      .signInWithPopup(provider)
+      .catch((error) => window.alert(error))
+    this.setUserData(result?.user as UserData)
+    this.router.navigate(['products'])
+    return result
+  }
+  setUserData(user: UserData) {
+    const userRef: AngularFirestoreDocument<UserData> = this.afs.doc(
       `users/${user.uid}`
     )
     const userData: User = {
