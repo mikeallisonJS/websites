f22721de8d573ce1f6aa4d0401ef37114912cfe8
Mike Allison <dj.mikeallison@gmail.com>
2023-03-27 06:14:00 +0000

    fix aweber

--- packages/offer.captainssounds.com/functions/src/index.ts
+++ packages/offer.captainssounds.com/functions/src/index.ts
@@ -1,24 +1,25 @@
 import * as functions from 'firebase-functions'
 import fetch from 'node-fetch'
+import * as cors from 'cors'
+
+const corsHelper = cors({ origin: 'https://offer.captainssounds.com' })
 
 export const aweberSignUp = functions.https.onRequest((request, response) => {
-  response.set(
-    'Access-Control-Allow-Origin',
-    'https://offer.captainssounds.com'
-  )
-  const { name, email } = request.body
-  fetch('https://www.aweber.com/scripts/addlead.pl', {
-    method: 'POST',
-    body: JSON.stringify({
-      name,
-      email,
-      listname: 'captainssounds'
+  corsHelper(request, response, () => {
+    const { name, email } = request.body
+    fetch('https://www.aweber.com/scripts/addlead.pl', {
+      method: 'POST',
+      body: JSON.stringify({
+        name,
+        email,
+        listname: 'captainssounds'
+      })
+    }).then((result) => {
+      if (result.ok) {
+        response.send({ result: true })
+      } else {
+        response.send({ result: false })
+      }
     })
-  }).then((result) => {
-    if (result.ok) {
-      response.send(true)
-    } else {
-      response.send(false)
-    }
   })
 })
