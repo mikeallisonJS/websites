2d1dd2eaa25b9726413a583d467fae71afa445a1
Mike Allison <dj.mikeallison@gmail.com>
2023-03-27 06:13:55 +0000

    fix aweber

--- packages/offer.captainssounds.com/functions/src/index.ts
+++ packages/offer.captainssounds.com/functions/src/index.ts
@@ -2,6 +2,10 @@ import * as functions from 'firebase-functions'
 import fetch from 'node-fetch'
 
 export const aweberSignUp = functions.https.onRequest((request, response) => {
+  response.set(
+    'Access-Control-Allow-Origin',
+    'https://offer.captainssounds.com'
+  )
   const { name, email } = request.body
   fetch('https://www.aweber.com/scripts/addlead.pl', {
     method: 'POST',
