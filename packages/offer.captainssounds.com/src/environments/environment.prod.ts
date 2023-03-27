1cb191ed73651a34b9ba630072482af7a879956c
Mike Allison <dj.mikeallison@gmail.com>
2023-03-27 05:09:53 +0000

    fix aweber

--- packages/offer.captainssounds.com/src/environments/environment.prod.ts
+++ packages/offer.captainssounds.com/src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'offer-captainssounds-com',
+    appId: '1:994045629011:web:a82dc0daa8dacca44eeca6',
+    storageBucket: 'offer-captainssounds-com.appspot.com',
+    apiKey: 'AIzaSyAw6TD-odiDPQL1E5e-kBQ5iRKpDVj2vws',
+    authDomain: 'offer-captainssounds-com.firebaseapp.com',
+    messagingSenderId: '994045629011',
+    measurementId: 'G-KG6YDQ0FPL'
+  },
   production: true,
   useEmulators: false
 }
