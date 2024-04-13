0fcad21bec4e536c508069431c3f244790d8e1f6
Mike Allison <dj.mikeallison@gmail.com>
2024-04-13 10:23:32 +0000

    csupdate

--- packages/captainssounds.com/tailwind.config.js
+++ packages/captainssounds.com/tailwind.config.js
@@ -2,7 +2,7 @@ const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/app/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
