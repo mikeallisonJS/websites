fb6df2a35b6dbfc5f41b4490bdc73bf9e298ef4c
Mike Allison <dj.mikeallison@gmail.com>
2024-04-20 07:11:42 +0000

    sentry

---
+++ packages/captainssounds.com/sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: 'https://55bf9e6de9a205417969a4cbc036783e@o4507117455015936.ingest.us.sentry.io/4507117457702912',
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+})
--- packages/captainssounds.com/src/app/error.tsx
+++ packages/captainssounds.com/src/app/error.tsx
@@ -1,6 +1,20 @@
 'use client'
 
-export default function Error({ reset }: { reset: () => void }) {
+import { useEffect } from 'react'
+import * as Sentry from '@sentry/nextjs'
+
+export default function Error({
+  reset,
+  error
+}: {
+  reset: () => void
+  error: Error
+}) {
+  useEffect(() => {
+    // Log the error to Sentry
+    Sentry.captureException(error)
+  }, [error])
+
   return (
     <div className="mx-auto my-4 flex max-w-xl flex-col rounded-lg border border-neutral-200 bg-white p-8 md:p-12 dark:border-neutral-800 dark:bg-black">
       <h2 className="text-xl font-bold">Oh no!</h2>
