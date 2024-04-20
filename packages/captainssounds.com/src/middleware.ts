e02bed230dbf6a6fbc3a3ddf28133ab645c03d36
Mike Allison <dj.mikeallison@gmail.com>
2024-04-20 02:14:47 +0000

    webhooks

--- packages/captainssounds.com/src/middleware.ts
+++ packages/captainssounds.com/src/middleware.ts
@@ -7,5 +7,5 @@ export default clerkMiddleware((auth, req) => {
 })
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)']
+  matcher: ['/((?!.*\\..*|_next|webhooks).*)', '/', '/(api|trpc)(.*)']
 }
