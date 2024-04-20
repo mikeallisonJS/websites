ab58435e922091797fda1832f809c90aece29581
Mike Allison <dj.mikeallison@gmail.com>
2024-04-20 05:56:18 +0000

    remove angular members

--- packages/members.captainssounds.com/src/app/uat/get-started/get-started.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'app-uat-get-started',
-  templateUrl: './get-started.component.html',
-  styleUrls: ['./get-started.component.scss']
-})
-export class GetStartedComponent {}
