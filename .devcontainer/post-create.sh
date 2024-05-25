375a420e124202f330bed2c95747770f89448214
Mike Allison <dj.mikeallison@gmail.com>
2024-05-25 04:30:57 -0700

    bun (#89)

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,5 @@
-pnpm install
-npm i -g prettier @withgraphite/graphite-cli@stable
+bun install
+bun install -g prettier @withgraphite/graphite-cli@stable
 
 # Remove the nx-native-file-cache folder to avoid permission issues with the nx cli
 sudo rm -rf /tmp/nx-native-file-cache/
