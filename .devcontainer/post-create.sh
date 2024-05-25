29a32867016b8b38298b007eb15bee0e60d0e7a7
Mike Allison <dj.mikeallison@gmail.com>
2024-05-25 07:30:58 +0000

    docker compose

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,3 +1,5 @@
 pnpm install
 npm i -g prettier @withgraphite/graphite-cli@stable
 
+# Remove the nx-native-file-cache folder to avoid permission issues with the nx cli
+sudo rm -rf /tmp/nx-native-file-cache/
