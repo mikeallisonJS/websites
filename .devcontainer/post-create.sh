6f5a160cec3f1a57b241e48b95a762de1508a5b9
Mike Allison <dj.mikeallison@gmail.com>
2024-05-25 07:12:35 +0000

    dockerfile cleanup

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,3 @@
 pnpm install
 npm i -g prettier @withgraphite/graphite-cli@stable
 
-sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
-curl -sLf --retry 3 --tlsv1.2 --proto "=https" 'https://packages.doppler.com/public/cli/gpg.DE2A7741A397C129.key' | sudo apt-key add -
-echo "deb https://packages.doppler.com/public/cli/deb/debian any-version main" | sudo tee /etc/apt/sources.list.d/doppler-cli.list
-sudo apt-get update && sudo apt-get install -y doppler jq
