26dd4e434ceb2fa936c05ba5630a41be97dd6411
Mike Allison <dj.mikeallison@gmail.com>
2024-06-11 09:11:11 +0000

    react email

--- packages/captainssounds.com/src/components/orders.tsx
+++ packages/captainssounds.com/src/components/orders.tsx
@@ -1,5 +1,3 @@
-'use server'
-
 import { currentUser } from '@clerk/nextjs/server'
 import Link from 'next/link'
 
@@ -12,8 +10,7 @@ import {
   TableHeader,
   TableRow
 } from '@websites/shared/react/components'
-
-import { db } from '../lib/drizzle'
+import { getOrdersForEmailOrUser } from '../getOrders'
 
 export async function Orders() {
   const user = await currentUser()
@@ -21,39 +18,7 @@ export async function Orders() {
     return null
   }
 
-  const orders = await db.query.order.findMany({
-    where: (order, { inArray }) =>
-      inArray(
-        order.email,
-        user.emailAddresses.map(({ emailAddress }) => emailAddress)
-      ),
-    with: {
-      orderToProducts: {
-        with: {
-          product: {
-            with: {
-              download: true
-            }
-          }
-        }
-      }
-    }
-  })
-
-  const hasUatPurchase =
-    orders.filter(
-      ({ orderToProducts }) =>
-        orderToProducts.filter(
-          ({ product }) => product.id == 'ultimate-ableton-templates'
-        ).length > 0
-    ).length > 0
-
-  const uatProducts = hasUatPurchase
-    ? await db.query.product.findMany({
-        with: { download: true }
-      })
-    : []
-  const uatLinks = hasUatPurchase ? await db.query.link.findMany() : []
+  const { orders, products, links } = await getOrdersForEmailOrUser({ user })
 
   return orders.length === 0 ? (
     <div>No orders found</div>
@@ -67,33 +32,18 @@ export async function Orders() {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {hasUatPurchase
-          ? uatProducts.map((product) => (
-              <TableRow key={product.id}>
-                <TableCell className="font-medium">{product.name}</TableCell>
-                <TableCell>
-                  {product.download != null ? (
-                    <Link href={product.download.url}>Download</Link>
-                  ) : (
-                    'Error'
-                  )}
-                </TableCell>
-              </TableRow>
-            ))
-          : orders.map((order) =>
-              order.orderToProducts.map(({ product }) => (
-                <TableRow key={product.id}>
-                  <TableCell className="font-medium">{product.name}</TableCell>
-                  <TableCell>
-                    {product.download != null ? (
-                      <Link href={product.download.url}>Download</Link>
-                    ) : (
-                      'Error'
-                    )}
-                  </TableCell>
-                </TableRow>
-              ))
-            )}
+        {products.map((product) => (
+          <TableRow key={product.id}>
+            <TableCell className="font-medium">{product.name}</TableCell>
+            <TableCell>
+              {product.download != null ? (
+                <Link href={product.download.url}>Download</Link>
+              ) : (
+                'Error'
+              )}
+            </TableCell>
+          </TableRow>
+        ))}
       </TableBody>
     </Table>
   )
---
+++ packages/captainssounds.com/src/lib/mailgun/order.ts
@@ -0,0 +1,56 @@
+import formData from 'form-data'
+import Mailgun from 'mailgun.js'
+const mailgun = new Mailgun(formData)
+import { render } from '@react-email/render'
+import OrderSuccess from '../../emails/orderSuccess'
+const mg = mailgun.client({
+  username: 'api',
+  key: process.env.MAILGUN_API_KEY as string
+})
+
+interface sendEmailProps {
+  firstName: string
+  email: string
+  productDownloads: Array<{
+    name: string
+    url: string
+  }>
+}
+
+export function sendEmail({
+  firstName,
+  email,
+  productDownloads
+}: sendEmailProps) {
+  const html = render(
+    OrderSuccess({
+      firstName,
+      email,
+      productDownloads
+    }),
+    {
+      pretty: true
+    }
+  )
+
+  const text = render(
+    OrderSuccess({
+      firstName,
+      email,
+      productDownloads
+    }),
+    {
+      plainText: true
+    }
+  )
+  mg.messages
+    .create('orders.captainssounds.com', {
+      from: 'Captain <captain@orders.captainssounds.com>',
+      to: [email],
+      subject: `${firstName}, thank you for your purchase. Here are your download links.`,
+      text,
+      html
+    })
+    .then((msg) => console.log(msg)) // logs response data
+    .catch((err) => console.log(err)) // logs any error
+}
