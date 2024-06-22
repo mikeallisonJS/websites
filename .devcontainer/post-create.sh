d416bf5dcbb142b1a6141c85c5e667dcd512ad28
Mike Allison <dj.mikeallison@gmail.com>
2024-06-22 15:20:55 -0700

    bun 1.1.15 (#100)

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,9 @@
+curl -fsSL https://bun.sh/install | bash -s "bun-v1.1.15"
+git config diff.lockb.textconv bun
+git config diff.lockb.binary true
+
 bun install
-bun install -g prettier @withgraphite/graphite-cli@stable
+bun install -g @withgraphite/graphite-cli@stable
 
 # Remove the nx-native-file-cache folder to avoid permission issues with the nx cli
 sudo rm -rf /tmp/nx-native-file-cache/
