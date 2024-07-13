9aee2758a319a7fe5b772d36897a2037fba00585
Mike Allison <dj.mikeallison@gmail.com>
2024-07-13 09:56:31 +0000

    new  stripe

--- apps/captainssounds.com/src/lib/stripe/index.ts
+++ apps/captainssounds.com/src/lib/stripe/index.ts
@@ -4,7 +4,7 @@ import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   // https://github.com/stripe/stripe-node#configuration
-  apiVersion: '2024-04-10',
+  apiVersion: '2024-06-20',
   appInfo: {
     name: 'captainssounds',
     url: 'https://captainssounds.com'
