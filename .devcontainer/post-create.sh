44bb9935f0eb9ca5b861a0fca512da8d8b61c45c
Mike Allison <dj.mikeallison@gmail.com>
2025-01-11 11:36:59 -0800

    bun txt lock (#233)

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,5 @@
 sudo chown -R node:node /home/node
 
-curl -fsSL https://bun.sh/install | bash -s "bun-v1.1.38"
-
-git config diff.lockb.textconv bun
-git config diff.lockb.binary true
+curl -fsSL https://bun.sh/install | bash -s "bun-v1.1.43"
 
 bun install
