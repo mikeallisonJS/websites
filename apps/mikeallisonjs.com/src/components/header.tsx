90515d5cff3d7c29ff70a98672e2d1a8a2cf853c
Mike Allison <dj.mikeallison@gmail.com>
2025-04-17 04:39:11 +0000

    tw

--- apps/mikeallisonjs.com/src/components/header.tsx
+++ apps/mikeallisonjs.com/src/components/header.tsx
@@ -6,29 +6,29 @@ export default function Header() {
     <header className="border-border/40 bg-background/55 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur-sm">
       <div className="container flex h-12 max-w-screen-2xl items-center py-4">
         <div className="flex w-full">
-          <Link href="/" className="ml-8 mr-auto flex items-center text-3xl">
+          <Link href="/" className="ml-8 mr-auto flex items-center text-3xl text-primary">
             mikeallison<span className="text-secondary">JS</span>
           </Link>
           <nav className="mr-8 hidden items-center justify-end gap-4 text-sm font-medium md:flex lg:gap-6">
             <Link
               href="#portfolio"
-              className="underline-offset-4 hover:underline"
+              className="underline-offset-4 hover:underline text-primary"
             >
               Portfolio
             </Link>
             <Link
               href="#services"
-              className="underline-offset-4 hover:underline"
+              className="underline-offset-4 hover:underline text-primary"
             >
               Services
             </Link>
             <Link
               href="#contact"
-              className="underline-offset-4 hover:underline"
+              className="underline-offset-4 hover:underline text-primary"
             >
               Contact
             </Link>
-            <Link href="https://github.com/mikeallisonJS" target="_blank">
+            <Link href="https://github.com/mikeallisonJS" target="_blank" className="text-primary">
               <IconBrandGithub />
             </Link>
           </nav>
--- apps/sltdnb.com/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from 'tailwindcss/dist/lib.mjs'
-import { tailwindConfig } from '@mikeallisonjs/shared-react-components'
-
-const config: Config = {
-  ...tailwindConfig
-}
-
-export default config
